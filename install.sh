98f6a3daa3d57c517058949e9d6107fefaa3af95
Mohammad Gufran <mohammad.gufran@kayako.com>
2015-11-22 13:16:19 +0530

    Install script detect piping to sh
    If the install script is piped to sh - that is, if the user is
    trying to install the configuration with curl or wget - it will
    detect the operation and clone down the configuration before
    proceeding, moving existing configuration to a backup directory

--- install.sh
+++ install.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-echo "Installing Vundle as default plugin manager. hang on."
-git clone https://github.com/gmarik/Vundle.vim $HOME/.vim/bundle/Vundle.vim &> /dev/null
-vim +VundleInstall +qa
+# If this script is piped to bash instead of direct execution, it must clone
+# the configuration to .vim directory before it can proceed.
+if [ ! -t 0 ]; then
+    if [ -e $HOME/.vim ]; then
+        echo "Moving existing vim configuration to $HOME/.vim_old"
+        mv $HOME/.vim $HOME/.vim_old
+    fi
 
-echo "Making Vimproc - "
-cd $HOME/.vim/bundle/vimproc/ && make &> /dev/null && cd - &> /dev/null
+    echo "Downloading new vim configuration..."
+    git clone https://github.com/Gufran/vimconfig $HOME/.vim 1> /dev/null
+fi
+
+echo "Installing vim-plug as default plugin manager. hang on..."
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &> /dev/null
+
+echo "Installing plugins..."
+vim --noplugin -c "source ~/.vim/autoload/plug.vim" +PlugInstall +qa - > /dev/tty
 
 if [ -f $HOME/.vimrc ]; then 
     mv $HOME/.vimrc $HOME/.vimrc_old
     echo "Your previous vimrc file has been moved from $HOME/.vimrc to $HOME/.vimrc_old"
-    echo "You might want to manually copy over the config you want from older file."
 fi
 
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
